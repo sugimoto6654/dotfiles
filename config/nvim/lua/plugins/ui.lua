eb8a7599775de98df8cf96fcea642b5fbe095885
sugimoto6654 <sugimoto.hinata.q2@dc.tohoku.ac.jp>
2026-07-12 14:03:11 +0900

    Add image.nvim support for WezTerm

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -1,3 +1,28 @@
 return {
-  
+  {
+    "3rd/image.nvim",
+    -- 画像ファイルを直接開いた場合にも表示を差し替えるため、起動時に読み込む
+    lazy = false,
+    opts = {
+      -- WezTerm では Kitty Graphics Protocol が完全には対応されていないため、
+      -- 安定して利用できる Sixel を使う
+      backend = "sixel",
+      processor = "magick_cli",
+      integrations = {
+        markdown = {
+          enabled = true,
+          only_render_image_at_cursor = true,
+          only_render_image_at_cursor_mode = "popup",
+        },
+      },
+      hijack_file_patterns = {
+        "*.png",
+        "*.jpg",
+        "*.jpeg",
+        "*.gif",
+        "*.webp",
+        "*.avif",
+      },
+    },
+  },
 }
